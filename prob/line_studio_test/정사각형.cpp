32f50d6663bba01f365b84f357abb5ba26808ee3
peter <peterhyun1234@gmail.com>
2019-12-19 22:32:38 +0900

    for practicing line coding test

---
+++ prob/line_studio_test/정사각형.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(vector<vector<int> > v) {
+    vector<int> ans;
+
+    for (vector<int>::size_type i = 0; i < v.size(); ++i){
+        
+    }
+    cout << "Hello Cpp" << endl;
+
+    return ans;
+}
