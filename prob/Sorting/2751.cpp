6d735a7c7d7de6b01fe4a82023864adbbe5caee9
HyunBin_Jeon <peterhyun1234@gmail.com>
2020-07-07 16:53:03 +0900

    add 2751

--- prob/Sorting/2751.cpp
+++ prob/Sorting/2751.cpp
@@ -1,1 +1,28 @@
-aa
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+
+	ios_base::sync_with_stdio(false); // for 
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n;
+    cin >> n;
+
+    vector<int> num(n);
+	
+    for(int i = 0; i < n; i++) 
+        cin >> num[i];
+
+	sort(num.begin(), num.end());
+
+    for(int j = 0; j < n; j++) 
+        cout << num[j] << "\n";
+
+    return 0;
+
+}
